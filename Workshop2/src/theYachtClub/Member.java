f2bf4cc7f62ee2fe3e1ddadac39efc32f8c6d2ed
amelow <amelielowe96@gmail.com>
2019-09-25 13:36:38 +0200

    Getters and setters done
    member class

--- Workshop2/src/theYachtClub/Member.java
+++ Workshop2/src/theYachtClub/Member.java
@@ -4,10 +4,49 @@ public class Member {
 	private String name;
 	private String personNum;
 	private int amountOfBoats;
-	private int id;
+	private int ID;
 
+	/*
+	 * Constructors
+	 */
 	public Member() {
+	}
 
+	// Getter and Setter for Name
+	public void setName(String Name) {
+		this.name = Name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	// Getter and Setter for Personal number
+	public void setPersonNum(String num) {
+		this.personNum = num;
+
+	}
+
+	public String getPersonNum() {
+		return personNum;
+	}
+
+	// Getter and Setter for The amount of Boats
+	public void setAmountOfBoats(int amount) {
+		this.amountOfBoats = amount;
+	}
+
+	public int getAmountOfBoats() {
+		return amountOfBoats;
+	}
+
+	// Getter and Setter for Id number
+	public void setID(int id) {
+		this.ID = id;
+	}
+
+	public int getID() {
+		return ID;
 	}
 
 }
