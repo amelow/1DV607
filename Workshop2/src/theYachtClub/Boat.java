c2ecd8d948b32e61e29209593bbc7314ed1d5ef5
amelow <amelielowe96@gmail.com>
2019-09-25 11:20:03 +0200

    getters and setters for length done

--- Workshop2/src/theYachtClub/Boat.java
+++ Workshop2/src/theYachtClub/Boat.java
@@ -1,10 +1,25 @@
 package theYachtClub;
 
 public class Boat {
+	private int length;
+	private String type;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public Boat() {
 
 	}
+
+	public Boat(String Type, int Length) {
+		this.type = Type;
+		this.length = Length;
+	}
+
+	public void setLength(int Length) {
+		this.length = Length;
+	}
+
+	public int getLength() {
+		return this.length;
+	}
+	
 
 }
