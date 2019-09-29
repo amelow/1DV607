84e132f3fbfc330edf64e757e137167c086105c1
amelow <amelielowe96@gmail.com>
2019-09-29 16:47:54 +0200

    added array list for members

--- Workshop2/src/theYachtClub/MemberRegister.java
+++ Workshop2/src/theYachtClub/MemberRegister.java
@@ -1,5 +1,24 @@
 package theYachtClub;
 
-public class MemberRegister {
+import java.util.ArrayList;
 
+public class MemberRegister {
+	private long id;
+	private ArrayList<Member> memList = new ArrayList<Member>();
+
+	public void AddMember() {
+
+	}
+
+	public void DeleteMember() {
+
+	}
+
+	public void GetMember() {
+
+	}
+
+	public void SaveMember() {
+
+	}
 }
