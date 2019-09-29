181c3c4e825ac56514d053445c22f473f2e4c0cd
amelow <amelielowe96@gmail.com>
2019-09-29 17:16:57 +0200

    add method done

--- Workshop2/src/theYachtClub/MemberRegister.java
+++ Workshop2/src/theYachtClub/MemberRegister.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 
 public class MemberRegister {
 	private long id;
+	private int amountOfMembers;
 	private ArrayList<Member> memList = new ArrayList<Member>();
 
-	public void AddMember() {
+	public void AddMember(Member member) {
+		memList.add(member);
+		amountOfMembers++;
 
 	}
 
