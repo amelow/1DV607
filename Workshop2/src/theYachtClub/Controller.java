1e74109ea4cab52ca923c9636a67edc606b3c79e
amelow <amelielowe96@gmail.com>
2019-09-30 10:57:37 +0200

    name change in controller

--- Workshop2/src/theYachtClub/Controller.java
+++ Workshop2/src/theYachtClub/Controller.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Controller {
 	private String userIn;
 	private Scanner scan = new Scanner(System.in);
-	public static View v = new View();
+	public static View view = new View();
 	public static MemberRegister MR = new MemberRegister();
 
 	/**
@@ -13,68 +13,43 @@ public class Controller {
 	 */
 
 	public void welcomeM() {
-		v.welcome();
+		view.welcome();
 		startM();
 	}
 
 	public void startM() {
-		v.mainMenu();
+		view.mainMenu();
 		userIn = scan.next();
-		switch(userIn) {
-		case("1"):
+		switch (userIn) {
+		case ("1"):
 			System.out.println("add mem");
-		break;
-		case("2"):
+			break;
+		case ("2"):
 			System.out.println("Change mem");
-		break;
-		case("3"):
+			break;
+		case ("3"):
 			System.out.println("compact");
-		break;
-		case("4"):
+			break;
+		case ("4"):
 			System.out.println("Verbose");
-		break;
-		case("Q"):
+			break;
+		case ("Q"):
 			System.out.println("quit");
 		}
-		
-		
-		
-	/*	switch(userIn) {
-			case("1"):
-			if(MR.getMemberList() == 0) {
-				v.error();
-				v.mainMenu();
-				}
-				else {
-					v.compactList();
-					v.mainMenu();
-				}
-				break;
-			
-			case("2"):
-				if(MR.getMemberList() == 0) {
-					v.error();
-					v.mainMenu();
-					}
-				else {
-					v.verboseList();
-					v.mainMenu;
-				}
-				break;
-			
-			case("3"):
-				v.addMember();
-				break;
-			
-			case("Q") :
-				v.saveQuit();
-				break;
-			
-			default:
-				v.invalid();
-				startM();
-				break;
-			}*/
-		}
+
+		/*
+		 * switch(userIn) { case("1"): if(MR.getMemberList() == 0) { v.error();
+		 * v.mainMenu(); } else { v.compactList(); v.mainMenu(); } break;
+		 * 
+		 * case("2"): if(MR.getMemberList() == 0) { v.error(); v.mainMenu(); } else {
+		 * v.verboseList(); v.mainMenu; } break;
+		 * 
+		 * case("3"): v.addMember(); break;
+		 * 
+		 * case("Q") : v.saveQuit(); break;
+		 * 
+		 * default: v.invalid(); startM(); break; }
+		 */
+	}
 
 }
