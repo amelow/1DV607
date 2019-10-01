5f80ff22dcdd0efa7589aa8cbfe98d271e85f48e
amelow <amelielowe96@gmail.com>
2019-10-01 20:04:48 +0200

    start save and load files

--- Workshop2/src/theYachtClub/Main.java
+++ Workshop2/src/theYachtClub/Main.java
@@ -1,8 +1,15 @@
 package theYachtClub;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Main {
 
 	public static void main(String[] args) {
+		/*
+		 * try { FileInputStream file = new FileInputStream("file.txt"); } catch
+		 * (FileNotFoundException e) { e.printStackTrace(); }
+		 */
 		Controller controller = new Controller();
 		controller.welcomeM();
 
