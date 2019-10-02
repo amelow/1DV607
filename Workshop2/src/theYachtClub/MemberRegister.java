a503ea347f675d3b92370a8b2bf55a7ed64378e7
amelow <amelielowe96@gmail.com>
2019-10-02 11:34:28 +0200

    Member register class done commenting

--- Workshop2/src/theYachtClub/MemberRegister.java
+++ Workshop2/src/theYachtClub/MemberRegister.java
@@ -2,75 +2,94 @@ package theYachtClub;
 
 import java.util.ArrayList;
 
+/*
+ * Member register handles all of the member-related actions,
+ * for example CreateMember,AddMember,DeleteMember, GetMember etc
+ */
 public class MemberRegister {
-	private int id = 1040;
+	// Declaring/Initiating the variables
+	private int id = 1040; // start at 1040 and not 1 so i cannot be so easily guessed
 	private int amountOfMembers;
 	private Member mem;
-	private ArrayList<Member> memList = new ArrayList<Member>();
+	private ArrayList<Member> memList = new ArrayList<Member>();// the memlist that saves the members
 
+	/*
+	 * Creating a member with a name and a personal number, then calling the add
+	 * member method
+	 */
 	public void CreateMember(String name, String num) {
 		Member member = new Member(name, num);
 		AddMember(member);
 	}
 
+	/*
+	 * Method that adds the new member to the list with a unique Id. Id starts at
+	 * 1040
+	 */
 	public void AddMember(Member member) {
 		member.setID(id);
 		memList.add(member);
-		amountOfMembers++;
-		id++;
+		amountOfMembers++; // incrementing the amount of members in the list
+		id++;// incrementing the Id so next member has a new ID
 	}
 
+	/*
+	 * Method that handles the delete member functionality
+	 */
 	public void DeleteMember(int number) {
-		memList.remove(number);
-		amountOfMembers--;
+		memList.remove(number);// removes the member with the right Id
+		amountOfMembers--;// decrementing the amount of members in the list
 
 	}
 
 	/*
-	 * “Compact List”; name, member id and number of boats
+	 * Method for printing the Compact List with the members Name, Member id and
+	 * Number of boats
 	 */
 	public void CompactList() {
-		for (int i = 0; i < memList.size(); i++) {
-			System.out.println("Name: " + memList.get(i).getName() + " " + "ID: " + memList.get(i).getID()+ " "+ "Boats: " + memList.get(i).getAmountOfBoats());
-			
+		for (int i = 0; i < memList.size(); i++) {// loops thru the register and prints the information
+			System.out.println("Name: " + memList.get(i).getName() + " " + "ID: " + memList.get(i).getID() + " "
+					+ "Boats: " + memList.get(i).getAmountOfBoats());
+
 		}
 
 	}
 
 	/*
-	 * “Verbose List”; name, personal number, member id and boats with boat
-	 * information
+	 * Method for printing the Verbose List with the members Name, Personal number,
+	 * Member id and Boats with boat information
 	 */
 	public void printVerbose() {
-		for (int i = 0; i < memList.size(); i++) {
+		for (int i = 0; i < memList.size(); i++) {// loops thru the register and prints the information
 			System.out.println("---------------------------------------");
-			System.out.println("Name: "+memList.get(i).getName());
-			System.out.println("Personal Number: "+memList.get(i).getPersonNum());
-			System.out.println("ID: "+memList.get(i).getID());
-			System.out.println("Number of Boats: "+memList.get(i).getAmountOfBoats());
-			if (memList.get(i).getAmountOfBoats() > 0) {
+			System.out.println("Name: " + memList.get(i).getName());
+			System.out.println("Personal Number: " + memList.get(i).getPersonNum());
+			System.out.println("ID: " + memList.get(i).getID());
+			System.out.println("Number of Boats: " + memList.get(i).getAmountOfBoats());
+			if (memList.get(i).getAmountOfBoats() > 0) {// checks if user has 1 or more boats
 				System.out.println("Registered Boats");
 				System.out.println("-----------------------------------");
-				for (int j = 0; j <memList.get(i).getAmountOfBoats(); j++) {
-					System.out.println("Type: "+memList.get(i).getBoatList().get(j).getType());
-					System.out.println("Length: "+memList.get(i).getBoatList().get(j).getLength());
-					System.out.println(" ");	
+				for (int j = 0; j < memList.get(i).getAmountOfBoats(); j++) { // loops thru the boats
+					System.out.println("Type: " + memList.get(i).getBoatList().get(j).getType());// prints all of the
+																									// boats information
+					System.out.println("Length: " + memList.get(i).getBoatList().get(j).getLength());
+					System.out.println(" ");
 				}
-				System.out.println("-----------------------------------");	
+				System.out.println("-----------------------------------");
 			}
 			System.out.println("----------------------------------------");
 		}
 
 	}
 
-	public long getID() {
+	public long getID() {// returns the members id
 		return id;
 
 	}
-	
-	public Member getMember(int id) {
+
+	public Member getMember(int id) { // get the member by the id
 		int index = -1;
-		for (int i = 0; i <memList.size(); i++) {
+		for (int i = 0; i < memList.size(); i++) {
 			Member testmem = memList.get(i);
 			if (testmem.getID() == id) {
 				index = i;
@@ -78,16 +97,14 @@ public class MemberRegister {
 		}
 		if (index == -1) {
 			return null;
-		}else {
+		} else {
 			return memList.get(index);
 		}
-		
-		
+
 	}
-	public ArrayList<Member> getMemberList() {
+
+	public ArrayList<Member> getMemberList() { // returns the entire member registry list
 		return memList;
 	}
-	
-	
 
 }
