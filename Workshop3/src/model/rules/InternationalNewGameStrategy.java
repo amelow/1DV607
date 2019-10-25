73962073828b823269cea6e177d05b584f90a07b
svampen125 <johan.e.1993@gmail.com>
2019-10-25 06:03:17 +0200

    removed duplicated code

--- Workshop3/src/model/rules/InternationalNewGameStrategy.java
+++ Workshop3/src/model/rules/InternationalNewGameStrategy.java
@@ -6,21 +6,26 @@ import model.Player;
 import model.Card;
 
 class InternationalNewGameStrategy implements INewGameStrategy {
+	private Dealer dealer;
 
 	public boolean NewGame(Deck a_deck, Dealer a_dealer, Player a_player) {
+		dealer = a_dealer;
 		Card c;
+		
+//		c = a_deck.GetCard();
+//		c.Show(true);
+//		a_player.DealCard(c);
+		dealer.DealCard(a_player,true);
+
+//		c = a_deck.GetCard();
+//		c.Show(true);
+//		a_dealer.DealCard(c);
+		dealer.DealCard(a_dealer,true);
 
 		c = a_deck.GetCard();
 		c.Show(true);
 		a_player.DealCard(c);
-
-		c = a_deck.GetCard();
-		c.Show(true);
-		a_dealer.DealCard(c);
-
-		c = a_deck.GetCard();
-		c.Show(true);
-		a_player.DealCard(c);
+		dealer.DealCard(a_player,true);
 
 		return true;
 	}
