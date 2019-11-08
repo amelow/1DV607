5a60db0e766a2a61be3af0af8fdc6b380c36071e
amelow <amelielowe96@gmail.com>
2019-11-08 20:49:23 +0100

    resolving issues

--- Workshop2/src/Controller/Controller.java
+++ Workshop2/src/Controller/Controller.java
@@ -350,9 +350,12 @@ public class Controller {
 			int counter = 0;
 			while ((line = buffReader.readLine()) != null) {
 				if (line.equals(",")) {
-					Member m = new Member(arr.get(0), arr.get(1), Integer.parseInt(arr.get(2))); // gets the name and the personal number from file
+
+					Member m = new Member(arr.get(0), arr.get(1), Integer.parseInt(arr.get(2))); // gets the name and
+																									// the personal
+																									// number from file
 					for (int i = 0; i < Integer.parseInt(arr.get(3)); i++) {
-						m.addBoat(BoatTypes.getBoatType(arr.get(4 + (counter))), // gets the boattypes fro file
+						m.addBoat(BoatTypes.getBoatType(arr.get(4 + (counter))), // gets the boattypes from file
 								Integer.parseInt(arr.get(5 + (counter))));
 						counter += 2;
 					}
