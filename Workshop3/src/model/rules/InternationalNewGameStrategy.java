0c7e197e4c50b965b5452a8d58f5ad64be6e9c9f
amelow <amelielowe96@gmail.com>
2019-12-03 16:40:33 +0100

    removing duplicate code

--- Workshop3/src/model/rules/InternationalNewGameStrategy.java
+++ Workshop3/src/model/rules/InternationalNewGameStrategy.java
@@ -11,19 +11,8 @@ class InternationalNewGameStrategy implements INewGameStrategy {
 		dealer = a_dealer;
 		Card c;
 
-//		c = a_deck.GetCard();
-//		c.Show(true);
-//		a_player.DealCard(c);
 		dealer.DealCard(a_player, true);
-
-//		c = a_deck.GetCard();
-//		c.Show(true);
-//		a_dealer.DealCard(c);
 		dealer.DealCard(a_dealer, true);
-
-		// c = a_deck.GetCard();
-		// c.Show(true);
-		// a_player.DealCard(c);
 		dealer.DealCard(a_player, true);
 
 		return true;
