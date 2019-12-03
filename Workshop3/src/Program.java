b5421f83ff1280d1e667d7e3ed8b00e07b593ff3
amelow <amelielowe96@gmail.com>
2019-12-03 22:09:27 +0100

    comment

--- Workshop3/src/Program.java
+++ Workshop3/src/Program.java
@@ -2,7 +2,10 @@
 import model.Game;
 import view.*;
 import controller.*;
-
+/*
+ * Code provided by Tobias Ohlsson for the 1DV607 course.
+ * Changes are made by Amelie Löwe and Johan Eriksson
+ */
 public class Program {
 
 	public static void main(String[] a_args) {
--- Workshop3/src/view/SimpleView.java
+++ Workshop3/src/view/SimpleView.java
@@ -8,9 +8,9 @@ public class SimpleView implements IView {
 	private final char quit = 'q';
 
 	public void DisplayWelcomeMessage() {
-		for (int i = 0; i < 50; i++) {
-			System.out.print("\n");
-		}
+	//	for (int i = 0; i < 50; i++) {
+	///		System.out.print("\n");
+		//}
 		;
 		System.out.println("Hello Black Jack World");
 		System.out.println("Type 'p' to Play, 'h' to Hit, 's' to Stand or 'q' to Quit\n");
