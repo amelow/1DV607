5973ad5b96f31ae7295047f9d4a6b529bd03e7a9
amelow <amelielowe96@gmail.com>
2019-12-03 22:39:41 +0100

    testing the swedish view

--- Workshop3/src/Program.java
+++ Workshop3/src/Program.java
@@ -2,6 +2,7 @@
 import model.Game;
 import view.*;
 import controller.*;
+
 /*
  * Code provided by Tobias Ohlsson for the 1DV607 course.
  * Changes are made by Amelie Löwe and Johan Eriksson
@@ -10,7 +11,8 @@ public class Program {
 
 	public static void main(String[] a_args) {
 
-		IView v = new SimpleView(); // new SwedishView();
+		// IView v = new SimpleView(); // new SwedishView();
+		IView v = new SwedishView();
 		Game g = new Game(v);
 		PlayGame ctrl = new PlayGame();
 
