51a1159a451ccf731349e3de5afb8542adffca68
amelow <amelielowe96@gmail.com>
2020-01-05 21:13:21 +0100

    main menu

--- Workshop2/src/View/EnglishView.java
+++ Workshop2/src/View/EnglishView.java
@@ -1,5 +1,24 @@
 package View;
 
-public class EnglishView implements ViewInterface{
+public class EnglishView implements ViewInterface {
+
+	@Override
+	public void MainMenu() {
+		String choose = "What do you want to do?";
+		String alt1 = "To show the verboselist, enter()";
+		String alt2 = "To show the compactlist, enter()";
+		String alt3 = "To add a member, enter ()";
+		String alt4 = "To change member info, enter()";
+		String alt5 = "To save and quit the application, enter (Q)";
+		
+		System.out.println(choose + "\n");
+		System.out.println(alt1);
+		System.out.println(alt2);
+		System.out.println(alt3);
+		System.out.println(alt4);
+		System.out.println(alt5);
+
+	}
+
 
 }
--- Workshop2/src/View/SwedishView.java
+++ Workshop2/src/View/SwedishView.java
@@ -4,8 +4,20 @@ public class SwedishView implements ViewInterface {
 
 	@Override
 	public void MainMenu() {
-		// TODO Auto-generated method stub
-		
+		String choose = "Vad vill du göra?";
+		String alt1 = "För att visa verbose-listan, tryck()";
+		String alt2 = "För att visa compact-listan, tryck()";
+		String alt3 = "För att lägga till en medlem, tryck ()";
+		String alt4 = "För att ändra en medlems information, tryck()";
+		String alt5 = "För att spara och stänga av, tryck (Q)";
+
+		System.out.println(choose + "\n");
+		System.out.println(alt1);
+		System.out.println(alt2);
+		System.out.println(alt3);
+		System.out.println(alt4);
+		System.out.println(alt5);
+
 	}
 
 }
