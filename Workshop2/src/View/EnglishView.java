d96256341d76bec32d3d0f951efca88ce9795f33
amelow <amelielowe96@gmail.com>
2020-02-11 16:00:44 +0100

    Order changes in englishView to make it more consis

--- Workshop2/src/View/EnglishView.java
+++ Workshop2/src/View/EnglishView.java
@@ -30,145 +30,265 @@ public class EnglishView implements IView {
 
 	@Override
 	public MainMenuOptions mainMenu() {
-		return null;
+		String choose = "What do you want to do?";
+		String alt1 = "To show the verboselist, enter(a)";
+		String alt2 = "To show the compactlist, enter(b)";
+		String alt3 = "To add a member, enter (c)";
+		String alt4 = "To change member info, enter(d)";
+		String alt5 = "To save and quit the application, enter (q)";
+
+		System.out.println(choose + "\n");
+		System.out.println(alt1);
+		System.out.println(alt2);
+		System.out.println(alt3);
+		System.out.println(alt4);
+		System.out.println(alt5);
+		String userIn = scan.next();
+		switch (userIn) {
+		case ("a"):
+			return MainMenuOptions.showVerbose;
+		case ("b"):
+			return MainMenuOptions.showCompact;
+		case ("c"):
+			return MainMenuOptions.addMember;
+		case ("d"):
+			return MainMenuOptions.changeMember;
+		case ("q"):
+			return MainMenuOptions.quitApp;
+		}
+		return MainMenuOptions.others;
 	}
 
 	@Override
 	public ChangeMemberOptions changeMember() {
-		// TODO Auto-generated method stub
-		return null;
+		String choose = "What do you want to do?";
+		String alt1 = "To change your name, enter(a)";
+		String alt2 = "To delete a member, enter(b)";
+		String alt3 = "To add a boat, enter(c)";
+		String alt4 = "To delete a boat, enter(d)";
+		String alt5 = "To change the boat info, enter(e)";
+
+		System.out.println(choose + "\n");
+		System.out.println(alt1);
+		System.out.println(alt2);
+		System.out.println(alt3);
+		System.out.println(alt4);
+		System.out.println(alt5);
+		String userIn = scan.next();
+		switch (userIn) {
+		case ("a"):
+			return ChangeMemberOptions.changeName;
+		case ("b"):
+			return ChangeMemberOptions.deleteMember;
+		case ("c"):
+			return ChangeMemberOptions.addBoat;
+		case ("d"):
+			return ChangeMemberOptions.deleteBoat;
+		case ("e"):
+			return ChangeMemberOptions.changeBoat;
+		}
+		return ChangeMemberOptions.others;
 	}
 
 	@Override
 	public void listMembers() {
-		// TODO Auto-generated method stub
+		StringBuffer listOfMembers = new StringBuffer();
+		Iterator<Member> membersList = memberRegistry.getMemberList();
+		while (membersList.hasNext()) {
+			Member member = membersList.next();
+			String name = member.getName();
+			int id = member.getID();
+			listOfMembers.append("Name: " + name + " | ");
+			listOfMembers.append(" Member id: " + id);
+			listOfMembers.append("\n");
+		}
+		System.out.println(listOfMembers.toString());
 		
 	}
 
 	@Override
-	public void listMembersBoats(int memberIndex) {
-		// TODO Auto-generated method stub
+	public void listMembersBoats(int memberId) {
+		Member member = memberRegistry.getMember(memberId);
+		StringBuffer listOfBoats = new StringBuffer();
+		int i = 1;
+		Iterator<Boat> boats = member.getBoatList();
+		while (boats.hasNext()) {
+			Boat boat = boats.next();
+			listOfBoats.append(i++);
+			listOfBoats.append(" " + boat.getType());
+			listOfBoats.append(" " + boat.getLength());
+			listOfBoats.append("\n");
+		}
+		System.out.println(listOfBoats.toString());
 		
 	}
 
 	@Override
 	public void compactListView() {
-		// TODO Auto-generated method stub
+		Iterator<Member> membersList = memberRegistry.getMemberList();
+		StringBuffer compactList = new StringBuffer();
+		while (membersList.hasNext()) {
+			Member member = membersList.next();
+			compactList.append("Member Id: " + member.getID() + " | ");
+			compactList.append("Name: " + member.getName() + " | ");
+			compactList.append("Amount of Boats: " + member.getAmountOfBoats() + " | ");
+			compactList.append("\n");
+
+		}
+
+		System.out.println(compactList.toString());
 		
 	}
 
 	@Override
 	public void verboseListView() {
-		// TODO Auto-generated method stub
+		Iterator<Member> membersList = memberRegistry.getMemberList();
+		StringBuffer verboseList = new StringBuffer();
+		System.out.println("-----------------------------------------------------------------------\n");
+		while (membersList.hasNext()) {
+			Member member = membersList.next();
+			verboseList.append("Member Id: " + member.getID() + " | ");
+			verboseList.append("Name: " + member.getName() + " | ");
+			verboseList.append("Personal Number: " + member.getPersonNum() + " | ");
+			verboseList.append("Amount of Boats: " + member.getAmountOfBoats() + " | ");
+			verboseList.append("\n");
+			int i = 1;
+			Iterator<Boat> boatsList = member.getBoatList();
+			while (boatsList.hasNext()) {
+				Boat boat = boatsList.next();
+				verboseList.append(i++);
+				verboseList.append("\t Type of Boat: " + boat.getType() + " | ");
+				verboseList.append("\t Length of Boat: " + meter2Feet(boat.getLength()) + " ft");
+				verboseList.append("\n");
+			}
+			verboseList.append("\n");
+		}
+		System.out.println(verboseList.toString());
 		
 	}
 
 	@Override
 	public void addName() {
-		// TODO Auto-generated method stub
+		System.out.println("Please enter your name (end with enter and -1: )");
 		
 	}
 
 	@Override
 	public void changeName() {
-		// TODO Auto-generated method stub
+		System.out.println("Please enter your new name (end with enter and -1: )");
 		
 	}
 
 	@Override
 	public void addPersonNum() {
-		// TODO Auto-generated method stub
+		System.out.println("Please enter your personal-number: (YYYYMMDD-XXXX)");
 		
 	}
 
 	@Override
 	public double addBoatLength() {
-		// TODO Auto-generated method stub
-		return 0;
+		System.out.println("Length of boat? (In feet)");
+		double userIn = scan.nextDouble();
+		return userIn;
 	}
 
 	@Override
 	public void deletedBoat(boolean deletedBoat) {
-		// TODO Auto-generated method stub
+		if (deletedBoat)
+			System.out.println("Boat is deleted");
+		else
+			System.out.println("Boat is NOT deleted");
 		
 	}
 
 	@Override
 	public void changeBoat() {
-		// TODO Auto-generated method stub
+		System.out.println("Which boat should be changed? Select the number infront of the boat type");
 		
 	}
 
 	@Override
 	public void changeTheBoat(String whatToChange) {
-		// TODO Auto-generated method stub
+		System.out.println(whatToChange);
 		
 	}
 
 	@Override
 	public void changeBoatTypeLength() {
-		// TODO Auto-generated method stub
+		System.out.println("Set new type and length for the boat ");
 		
 	}
 
 	@Override
 	public void boatUpdated(boolean updated) {
-		// TODO Auto-generated method stub
-		
+		if (updated)
+			System.out.println("Boat is updated");
+		else
+			System.out.println("Boat is NOT updated");
 	}
 
 	@Override
 	public void memberSaved() {
-		// TODO Auto-generated method stub
+		String saved = "---The member is now saved---";
+		System.out.println(saved + "\n");
 		
 	}
 
 	@Override
 	public void memberDeleted() {
-		// TODO Auto-generated method stub
+		System.out.println("---The member gets deleted---");
+		System.out.println("\n");
 		
 	}
 
 	@Override
 	public String typeOfBoat() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println("Type of boat? Choose between: Sailboat, Motorsailer, Kayak, Canoe and Other");
+		String userIn = scan.next();
+		return userIn;
 	}
 
 	@Override
 	public void selectID() {
-		// TODO Auto-generated method stub
+		String chooseID = "Select the member ID of the member that should be changed";
+		System.out.println(chooseID);
 		
 	}
 
 	@Override
 	public void selectBoatToDelete() {
-		// TODO Auto-generated method stub
+		System.out.println("Which boat do you want to delete? Select the number in front of boat type");
 		
 	}
 
 	@Override
 	public void noChanges() {
-		// TODO Auto-generated method stub
+		String no = "No changes made, prints the list of members again";
+		System.out.println(no);
 		
 	}
 
 	@Override
 	public void noUser() {
-		// TODO Auto-generated method stub
+		String noUser = "No user with that MemID, select again";
+		System.out.println(noUser);
 		
 	}
 
 	@Override
 	public void wrongFormat() {
-		// TODO Auto-generated method stub
+		System.out.println("Wrong format!It should be like: YYYYMMDDXXXX");
 		
 	}
 
 	@Override
 	public void userExist() {
-		// TODO Auto-generated method stub
+		System.out.println("Person number exist - returns to start menu");
 		
+	}
+	private double meter2Feet(double boatLength) {
+		// boatLength = boatLength * 3.2808;
+		return boatLength;
 	}
 }
 	/*
