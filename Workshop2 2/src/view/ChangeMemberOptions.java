cef3258134925aa4379857db130e7d477327ab2e
amelow <amelielowe96@gmail.com>
2020-03-11 09:39:44 +0100

    Workshop2.1 finished

--- Workshop2 2/src/view/ChangeMemberOptions.java
+++
@@ -1,5 +0,0 @@
-package view;
-
-public enum ChangeMemberOptions {
-	changeName,deleteMember,addBoat,deleteBoat, changeBoat,others;
-}
--- Workshop2 2/src/view/MainMenuOptions.java
+++
@@ -1,7 +0,0 @@
-package view;
-
-public enum MainMenuOptions {
-		showVerbose, showCompact, addMember, changeMember, quitApp, others;
-
-}
-
